6e6b13195463700085d05b7381485cd0a539d8b0
Gustavo Godoy <cyberzone8.tech@gmail.com>
2025-01-09 15:49:21 -0300

    Actualizar proyectos

--- luvie/server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const { registerUser, authUser } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/register', registerUser);
-router.post('/login', authUser);
-
-module.exports = router;
